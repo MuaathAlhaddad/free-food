4e3bfef51b9116abff3521dd472a4c2baf520ccb
Muaadh Esmail Qaid Almrham <muaath2000@gmail.com>
2021-06-25 18:26:37 +0800

    fix(vuex): store actions.js

--- resources/js/stores/actions.js
+++ resources/js/stores/actions.js
@@ -1,17 +1,31 @@
-/*
-export const actions = {
-  async nuxtServerInit({ commit }) {
-    const res = await this.$axios.get("/api/current_user")
-    commit("SET_USER", res.data)
-  },
-
-  async logout({ commit }) {
-    const { data } = await this.$axios.get("/api/logout")
-    if (data.ok) commit("SET_USER", null)
-  },
-
-  async handleToken({ commit }, token) {
-    const res = await this.$axios.post("/api/stripe", token)
-    commit("SET_USER", res.data)
-  }
-} */
+export default {
+    addToCart (id) {
+        let data = {
+            id: id,
+            status: true
+        }
+        this.$store.commit('addToCart', id);
+        this.$store.commit('setAddedBtn', data);
+    },
+    removeFromCart (id) {
+        let data = {
+            id: id,
+            status: false
+        }
+        this.$store.commit('removeFromCart', id);
+        this.$store.commit('setAddedBtn', data);
+    },
+    saveToFavorite (id) {
+        this.$store.commit('addToFavourite', id);
+    },
+    removeFromFavourite (id) {
+        this.$store.commit('removeFromFavourite', id);
+    },
+    onSelectQuantity (id) {
+        let data = {
+            id: id,
+            quantity: this.selected
+        }
+        this.$store.commit('quantity', data);
+    }
+}
